8c7e78a7a246a7d673592d253a33361c7b406037
Moussatassia <34881748+Moussatassia@users.noreply.github.com>
2019-12-25 22:06:09 +0100

    Create __init__.py

---
+++ nutritions/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from . import patient
+from . import maladie
+from . import consultation
+from . import diagnostique
+from . import motifconsultation
+from . import prescription
+from . import medicament
+from . import infirmiere
+from . import medecin
+from . import nourriture
+from . import examen
+from . import rdv
+from . import regime
