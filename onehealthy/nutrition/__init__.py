9c96ee8b550eeff17049ceb3362caf00da5e5969
Moussatassia <34881748+Moussatassia@users.noreply.github.com>
2019-12-25 22:59:33 +0100

    Create __init__.py

---
+++ onehealthy/nutrition/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+from . import Type_nourriture
+from . import Nourriture
+from . import Composition_nourriture
+from . import Temps_repas
+from . import Regime
+from . import Regime_ligne
