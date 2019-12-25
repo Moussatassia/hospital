3e071c1fcaefe4501c019e9843308ceeff8ed37f
Moussatassia <34881748+Moussatassia@users.noreply.github.com>
2019-12-25 23:07:40 +0100

    Create __init__.py

---
+++ onehealthy/parametres/__init__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from . import Maladie_enfance
+from . import Profession
+from . import Parent
+from . import Diagnostic
+from . import Motif_consultation
+from . import Antecedent
+from . import Examen_biologique
+from . import Radiographie
+from . import Medicament
+from . import Lait
+from . import Examen_clinique
+from . import Examen_medical
+from . import Prescription_medicale
+from . import Historique_poids_taille
+from . import Medecin_traitant
