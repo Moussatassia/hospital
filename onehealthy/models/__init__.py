709b9f49a9feb89fb70f06c278c1cacdb5402cee
Moussatassia <34881748+Moussatassia@users.noreply.github.com>
2019-12-25 22:54:23 +0100

    Create __init__.py

---
+++ onehealthy/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+from . import Patient
+from . import Consultation
+from . import Certificat_medical
+from . import Rendez_vous
+from . import Attachement
+from . import Company
