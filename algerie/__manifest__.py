30c0fa9b872f60ddf2c9f6e391bffc2c99e0309b
Moussatassia <34881748+Moussatassia@users.noreply.github.com>
2019-12-25 18:58:33 +0100

    Create __manifest__.py

---
+++ algerie/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Wilayas d'Algérie",
+    "version": "1.0",
+    "author": "Moussa",
+    "website": "http://univ-tlemcen.dz",
+    "sequence": 0,
+    'category': 'Education',
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+    ],
+    "description": """
+
+	""",
+    'summary': "Wilayas d'Algérie",
+    "qweb": [
+    ],
+    "demo": [
+    ],
+    "images": [
+    ],
+    "data": [
+        'data/Wilaya.xml',
+        'data/Commune.xml', 
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+}
